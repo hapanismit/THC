30627a8cd44e09e979aa42c3751e7401904ff6b3
Smit Hapani <hapanismit@gmail.com>
2021-10-21 11:41:41 -0400

    Project Created

---
+++ src/test/java/com/egen/thc/ThcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egen.thc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
